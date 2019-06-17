ecdf0a2688a187c07069dbef8bf1085ae1555637
Mohit Jindal <ZJINMOH@ericsson.com>
2019-06-17 15:14:16 +0530

    SpringBootJDBCDatabaseInitialization

---
+++ src/main/java/com/infotech/app/SpringBootJpaDatabaseInitializationApplication.java
@@ -0,0 +1,37 @@
+package com.infotech.app;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.infotech.app.entities.Employee;
+import com.infotech.app.repository.EmployeeRepository;
+
+@SpringBootApplication
+public class SpringBootJpaDatabaseInitializationApplication implements CommandLineRunner {
+
+	
+	@Autowired
+	private EmployeeRepository employeeRepository;
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJpaDatabaseInitializationApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		Employee employee = getEmployee();
+		employeeRepository.createEmployee(employee);
+	}
+
+	private Employee getEmployee()
+	{
+		Employee employee = new Employee();
+		employee.setEmployeeName("Sean Murphy");
+		employee.setEmail("sean.m@gmail.com");
+		employee.setSalary(90000.00);
+		return employee;
+	}
+}
---
+++ src/main/java/com/infotech/app/repository/EmployeeRepository.java
@@ -0,0 +1,21 @@
+package com.infotech.app.repository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import com.infotech.app.entities.Employee;
+
+@Repository
+public class EmployeeRepository
+{
+
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+	
+	public void createEmployee(Employee employee)
+	{
+		String SQL = "insert into employee_table (email, employee_name, salary) values (?,?,?)";
+		jdbcTemplate.update(SQL, employee.getEmail(), employee.getEmployeeName(), employee.getSalary());
+	}
+}
